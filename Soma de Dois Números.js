4127854c32271dfbdad4c7bfdc8e8157238a8c36
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:23:04 -0300

    Update Soma de Dois Números.js

--- Soma de Dois Números.js
+++ Soma de Dois Números.js
@@ -1,1 +1,16 @@
+function somarNumeros() {
 
+    let numero1 = parseInt(prompt("Digite o primeiro número inteiro:"));
+    
+
+    let numero2 = parseInt(prompt("Digite o segundo número inteiro:"));
+    
+
+    let soma = numero1 + numero2;
+    
+
+    alert("A soma de " + numero1 + " e " + numero2 + " é " + soma);
+}
+
+
+somarNumeros();
