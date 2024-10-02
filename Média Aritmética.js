4dbbb3c33dbc84435b8057c4433d632322d6711c
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:18:51 -0300

    Update Média Aritmética.js

--- Média Aritmética.js
+++ Média Aritmética.js
@@ -1,1 +1,15 @@
-Média Aritmética
+function calcularMedia() {
+    // Lê as três notas do aluno
+    let nota1 = parseFloat(prompt("Digite a primeira nota:"));
+    let nota2 = parseFloat(prompt("Digite a segunda nota:"));
+    let nota3 = parseFloat(prompt("Digite a terceira nota:"));
+    
+
+    let media = (nota1 + nota2 + nota3) / 3;
+    
+
+    alert("A média aritmética das notas é " + media);
+}
+
+
+calcularMedia();
