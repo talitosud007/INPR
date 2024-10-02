5ae25d37a9b0c14a2e08f9cc3fe04074b71d917e
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:18:35 -0300

    Update Desconto em Compra.js

--- Desconto em Compra.js
+++ Desconto em Compra.js
@@ -1,1 +1,19 @@
-Desconto em Compra
+function calcularValorFinalComDesconto() {
+
+    let valorCompra = parseFloat(prompt("Digite o valor da compra:"));
+    
+
+    let percentualDesconto = parseFloat(prompt("Digite a porcentagem de desconto:"));
+    
+
+    let desconto = (valorCompra * percentualDesconto) / 100;
+    
+
+    let valorFinal = valorCompra - desconto;
+    
+
+    alert("O valor final da compra após o desconto é: R$ " + valorFinal.toFixed(2));
+}
+
+
+calcularValorFinalComDesconto();
