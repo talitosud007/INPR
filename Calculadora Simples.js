658477541614e1655a7f439851be6001fc7faacc
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:17:22 -0300

    Update Calculadora Simples.js

--- Calculadora Simples.js
+++ Calculadora Simples.js
@@ -1,1 +1,42 @@
-calculadora simples
+function calcular() {
+
+    let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+    
+
+    let numero2 = parseFloat(prompt("Digite o segundo número:"));
+    
+
+    let operador = prompt("Digite o operador (+, -, *, /):");
+    
+    let resultado;
+    
+
+    switch (operador) {
+        case '+':
+            resultado = numero1 + numero2;
+            break;
+        case '-':
+            resultado = numero1 - numero2;
+            break;
+        case '*':
+            resultado = numero1 * numero2;
+            break;
+        case '/':
+
+            if (numero2 === 0) {
+                alert("Erro: Divisão por zero!");
+                return;
+            }
+            resultado = numero1 / numero2;
+            break;
+        default:
+            alert("Operador inválido!");
+            return;
+    }
+    
+
+    alert("O resultado de " + numero1 + " " + operador + " " + numero2 + " é " + resultado);
+}
+
+
+calcular();
