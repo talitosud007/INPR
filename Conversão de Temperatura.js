2073a0f38103efd923c0e1d2df15d913781f324e
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:18:19 -0300

    Update Conversão de Temperatura.js

--- Conversão de Temperatura.js
+++ Conversão de Temperatura.js
@@ -1,1 +1,13 @@
-Conversão de Temperatura
+function converterCelsiusParaFahrenheit() {
+    // Lê a temperatura em Celsius
+    let celsius = parseFloat(prompt("Digite a temperatura em Celsius:"));
+    
+
+    let fahrenheit = (celsius * (9 / 5)) + 32;
+    
+
+    alert(celsius + " °C é igual a " + fahrenheit + " °F");
+}
+
+
+converterCelsiusParaFahrenheit();
