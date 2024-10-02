e9f35b53531af6ca3eb7916e8dcf8ad3bc79457b
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:17:59 -0300

    Update Conversão de Moeda.js

--- Conversão de Moeda.js
+++ Conversão de Moeda.js
@@ -1,1 +1,16 @@
-Conversão de Moeda
+function converterReaisParaDolares() {
+
+    let valorReais = parseFloat(prompt("Digite o valor em reais:"));
+    
+
+    let taxaCambio = parseFloat(prompt("Digite a taxa de câmbio (reais por dólar):"));
+    
+
+    let valorDolares = valorReais / taxaCambio;
+    
+
+    alert("O valor em dólares é: $ " + valorDolares.toFixed(2));
+}
+
+
+converterReaisParaDolares();
