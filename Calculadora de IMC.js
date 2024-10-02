ee288fe3c43c21920dca7c1ffd07d2918267d80d
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:16:46 -0300

    Update Calculadora de IMC.js

--- Calculadora de IMC.js
+++ Calculadora de IMC.js
@@ -1,1 +1,16 @@
-Calculadora de IMC
+function calcularIMC() {
+
+    let peso = parseFloat(prompt("Digite o seu peso em kg:"));
+    
+
+    let altura = parseFloat(prompt("Digite a sua altura em metros:"));
+    
+
+    let imc = peso / (altura ** 2);
+    
+
+    alert("O seu Índice de Massa Corporal (IMC) é: " + imc.toFixed(2));
+}
+
+
+calcularIMC();
