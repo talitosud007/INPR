756f6cf5b48245ed5fb5986cbab00f9516f26770
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:23:31 -0300

    Update Troco de Compras.js

--- Troco de Compras.js
+++ Troco de Compras.js
@@ -1,1 +1,21 @@
-Troco de Compras
+function calcularTroco() {
+
+    let valorCompra = parseFloat(prompt("Digite o valor total da compra:"));
+    
+
+    let valorPago = parseFloat(prompt("Digite o valor pago pelo cliente:"));
+    
+
+    let troco = valorPago - valorCompra;
+    
+
+    if (troco < 0) {
+        alert("O valor pago não é suficiente para cobrir a compra.");
+    } else {
+
+        alert("O troco a ser devolvido é: R$ " + troco.toFixed(2));
+    }
+}
+
+
+calcularTroco();
