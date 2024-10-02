82196bbf84a9b191de9e979eac0dcbd91585ce92
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:22:45 -0300

    Update Perímetro de um Retângulo.js

--- Perímetro de um Retângulo.js
+++ Perímetro de um Retângulo.js
@@ -1,1 +1,16 @@
-Perímetro de um Retângulo
+function calcularPerimetroRetangulo() {
+
+    let largura = parseFloat(prompt("Digite a largura do retângulo:"));
+    
+
+    let altura = parseFloat(prompt("Digite a altura do retângulo:"));
+    
+
+    let perimetro = 2 * (largura + altura);
+    
+
+    alert("O perímetro do retângulo é " + perimetro);
+}
+
+
+calcularPerimetroRetangulo();
