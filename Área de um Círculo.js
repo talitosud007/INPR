2e0e9c1f4514a8c3c70ccdf0cc5cb2a1e9aeb40c
TALES RAFAEL DA SILVA LOPES <trsl2@aluno.ifal.edu.br>
2024-10-02 14:16:23 -0300

    Update Área de um Círculo.js

--- Área de um Círculo.js
+++ Área de um Círculo.js
@@ -1,1 +1,16 @@
-Área de um Círculo
+function calcularAreaCirculo() {
+    // Lê o raio do círculo
+    let raio = parseFloat(prompt("Digite o raio do círculo:"));
+    
+
+    const pi = 3.14;
+    
+
+    let area = pi * (raio ** 2);
+    
+
+    alert("A área do círculo com raio " + raio + " é " + area);
+}
+
+
+calcularAreaCirculo();
